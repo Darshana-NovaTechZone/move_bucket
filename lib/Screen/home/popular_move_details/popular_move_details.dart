dcd00ca8235b10c53ab86e17c7822548de3432a0
Darshana <darshana@novatechzone.com>
2023-06-01 09:36:43 +0530

    2023/06/01

--- lib/Screen/home/popular_move_details/popular_move_details.dart
+++ lib/Screen/home/popular_move_details/popular_move_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:movie_bucket/constan/color.dart';
+import 'package:movie_bucket/widget/text.dart';
 
 import '../../../widget/radius_button.dart';
 
@@ -32,37 +33,257 @@ class _PopularMoveDetailsState extends State<PopularMoveDetails> {
                 fit: BoxFit.fill,
               )),
           Positioned(
-              width: width - 40,
-              // left: 20,
-              // // right: 0,
-              bottom: 0,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  alignment: Alignment.center,
-                  height: 70,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20), color: yellow),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            height: height,
+            width: width,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: height / 30,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: white,
+                        size: 40,
+                      )),
+                  SizedBox(
+                    height: height / 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(Icons.add),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(
+                              text: '2014',
+                              colorText: Colors.white54,
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal),
+                          CustomText(
+                              text: 'Fury',
+                              colorText: white,
+                              fontSize: 27,
+                              fontWeight: FontWeight.normal),
+                        ],
+                      ),
+                      CircleAvatar(
+                        radius: 27,
+                        backgroundColor: yellow,
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomText(
+                              text: '64',
+                              colorText: white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                          CustomText(
+                              text: 'Metascore',
+                              colorText: Colors.white54,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomText(
+                              text: '361',
+                              colorText: white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                          CustomText(
+                              text: 'Reviews',
+                              colorText: Colors.white54,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomText(
+                              text: '98',
+                              colorText: white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                          CustomText(
+                              text: 'Popularity',
+                              colorText: Colors.white54,
+                              fontSize: 23,
+                              fontWeight: FontWeight.normal),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CustomText(
+                          text: 'Action,Drama War',
+                          colorText: white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal),
+                      CustomText(
+                          text: '2h 14min',
+                          colorText: white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: yellow,
+                          ),
+                          CustomText(
+                              text: '7.6',
+                              colorText: white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                          text: 'Synopsis',
+                          colorText: Colors.white54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                      SizedBox(
+                        height: height / 60,
+                      ),
+                      CustomText(
+                          text:
+                              ' A grizzied tank coander akes toughh decisions as he and his crew fight their way accross Germaany in April.195',
+                          colorText: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomText(
+                          text: 'Director',
+                          colorText: Colors.white54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
                       SizedBox(
                         width: 20,
                       ),
-                      Text('ADD TO',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: black,
-                              fontFamily: 'Tajawal',
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold)),
+                      CustomText(
+                          text: 'Davide Ayesr',
+                          colorText: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
                     ],
                   ),
-                ),
-              ))
+                  SizedBox(
+                    height: height / 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomText(
+                          text: 'Writer',
+                          colorText: Colors.white54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                      SizedBox(
+                        width: 36,
+                      ),
+                      CustomText(
+                          text: 'Davide Ayesr',
+                          colorText: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustomText(
+                          text: 'Actor',
+                          colorText: Colors.white54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      CustomText(
+                          text: 'Davide Ayesr',
+                          colorText: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal),
+                    ],
+                  ),
+                  SizedBox(
+                    height: height / 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: height / 17,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: yellow),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.add),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text('ADD TO',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: black,
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ]),
       ),
     );
